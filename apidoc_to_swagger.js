bed76f22e5a406f9dce79f3967f9cb4f6880e2d4
iZucken <izucken@gmail.com>
2022-02-26 06:16:00 +0300

    Update apidoc_to_swagger.js
    Added a lot of openapi 3.0 conversions

--- apidoc_to_swagger.js
+++ apidoc_to_swagger.js
@@ -1,11 +1,9 @@
 var _ = require('lodash');
-var { pathToRegexp } = require('path-to-regexp');
 const { debug, log } = require('winston');
 const GenerateSchema = require('generate-schema')
 
-
 var swagger = {
-    openapi: "3.0.0",
+    openapi: "3.0.3",
     info: {},
     paths: {}
 };
@@ -13,16 +11,7 @@ var swagger = {
 function toSwagger(apidocJson, projectJson) {
     swagger.info = addInfo(projectJson);
     swagger.paths = extractPaths(apidocJson);
-    // for (const key in swagger) {
-    //     console.log('[%s] %o', key, swagger[key]);
-    // }
     return swagger;
-}
-
-var tagsRegex = /(<([^>]+)>)/ig;
-// Removes <p> </p> tags from text
-function removeTags(text) {
-    return text ? text.replace(tagsRegex, "") : text;
 }
 
 function addInfo(projectJson) {
@@ -33,40 +22,94 @@ function addInfo(projectJson) {
     return info;
 }
 
-/**
- * Extracts paths provided in json format
- * post, patch, put request parameters are extracted in body
- * get and delete are extracted to path parameters
- * @param apidocJson
- * @returns {{}}
- */
 function extractPaths(apidocJson) {
-    var apiPaths = groupByUrl(apidocJson);
-    var paths = {};
-    for (var i = 0; i < apiPaths.length; i++) {
-        var verbs = apiPaths[i].verbs;
-        var url = verbs[0].url;
-        var pattern = pathToRegexp(url, null);
-        var matches = pattern.exec(url);
+	var paths = {};
+	let versionMap = {}
+	for (var i = 0; i < apidocJson.length; i++) {
+		var verb = apidocJson[i];
+		if (versionMap[verb.url + verb.type] && verb.version < versionMap[verb.url + verb.type]) {
+		continue
+		}
+		let urlStrip = /((\/[\w:]+)+)/.exec(verb.url)
+		var url = urlStrip[1];
+		var matchRegex = /(:\w+)\b/ig;
+		var pathKeys = {};
+		while (matches = matchRegex.exec(url)) {
+			for (let j = 1; j < matches.length; j++) {
+			    var key = matches[j].substr(1);
+			    pathKeys[key] = true;
+			    url = url.replace(matches[j], "{" + key + "}");
+			}
+		}
+		try {
+			var type = verb.type;
+			var obj = paths[url] = paths[url] || {};
+			_.extend(obj, generateProps(verb, pathKeys))
+			versionMap[verb.url + verb.type] = verb.version
+		} catch (error) {
+			console.error(url, type, error);
+			continue;
+		}
+	}
+	return paths;
+}
 
-        // Surrounds URL parameters with curly brackets -> :email with {email}
-        var pathKeys = [];
-        for (let j = 1; j < matches.length; j++) {
-            var key = matches[j].substr(1);
-            url = url.replace(matches[j], "{" + key + "}");
-            pathKeys.push(key);
-        }
-
-        for (let j = 0; j < verbs.length; j++) {
-            var verb = verbs[j];
-            var type = verb.type;
-
-            var obj = paths[url] = paths[url] || {};
-
-            _.extend(obj, generateProps(verb))
-        }
+function generateProps(verb, pathKeys) {
+    const pathItemObject = {}
+    const parameters = generateParameters(verb, pathKeys)
+    const responses = generateResponses(verb)
+    pathItemObject[verb.type] = {
+        tags: [verb.group],
+        summary: removeTags(verb.name) + ' v' + verb.version,
+        description: removeTags(verb.title),
+        parameters: parameters.parameters,
+        responses
     }
-    return paths;
+    if (verb.type !== 'get' && parameters.requestBody) {
+    pathItemObject[verb.type].requestBody = parameters.requestBody
+    }
+    return pathItemObject
+}
+
+function generateParameters(verb, pathKeys) {
+    const mixedQuery = []
+    const mixedBody = []
+    const parameters = []
+    const header = verb && verb.header && verb.header.fields.Header || []
+    if (verb && verb.parameter && verb.parameter.fields) {
+        const Parameter = verb.parameter.fields.Parameter || []
+        mixedQuery.push(...(verb.parameter.fields.Query || []))
+        mixedBody.push(...(verb.parameter.fields.Body || []))
+        Parameter.forEach(p => {
+        	if (pathKeys[p.field]) {
+        		parameters.push({
+				in: 'path',
+				name: p.field,
+				description: removeTags(p.description),
+				required: !p.optional,
+				schema: {type: p.type.toLowerCase()}
+			})
+			return
+        	}
+		if (verb.type === 'get') {
+			mixedQuery.push(p)
+		} else {
+			mixedBody.push(p)
+		}
+        })
+    }
+    parameters.push(...mixedQuery.map(p => {
+	    return {
+		in: 'query',
+		name: p.field,
+		description: removeTags(p.description),
+		required: !p.optional,
+		schema: {type: p.type.toLowerCase()}
+	    }
+    }))
+    parameters.push(...header.map(mapHeaderItem))
+    requestBody = generateRequestBody(verb, mixedBody)
+    return {parameters, requestBody}
 }
 
 function mapHeaderItem(i) {
@@ -79,172 +122,33 @@ function mapHeaderItem(i) {
         default: i.defaultValue
     }
 }
-function mapQueryItem(i) {
-    return {
-        type: 'string',
-        in: 'query',
-        name: i.field,
-        description: removeTags(i.description),
-        required: !i.optional,
-        default: i.defaultValue
-    }
+
+var tagsRegex = /(<([^>]+)>)/ig;
+
+function removeTags(text) {
+    return text ? text.replace(tagsRegex, "") : text;
 }
 
-const defaultBodyParameter = {
-    // name: 'root',
-    // in: 'body',
-    schema: {
-        properties: {},
-        type: 'object',
-        required: []
-    }
-}
-
-/**
- * apiDocParams
- * @param {type} type
- * @param {boolean} optional
- * @param {string} field
- * @param {string} defaultValue
- * @param {string} description
- */
-
-/**
- * 
- * @param {ApidocParameter[]} apiDocParams 
- * @param {*} parameter 
- */
-function transferApidocParamsToSwaggerBody(apiDocParams, parameterInBody) {
-
-    let mountPlaces = {
-        '': parameterInBody['schema']
-    }
-
-    apiDocParams.forEach(i => {
-        const type = i.type.toLowerCase()
-        const key = i.field
-        const nestedName = createNestedName(i.field)
-        const { objectName = '', propertyName } = nestedName
-
-        if (type.endsWith('object[]')) {
-            // if schema(parsed from example) doesn't has this constructure, init
-            if (!mountPlaces[objectName]['properties'][propertyName]) {
-                mountPlaces[objectName]['properties'][propertyName] = { type: 'array', items: { type: 'object', properties: {}, required: [] } }
-            }
-
-            // new mount point
-            mountPlaces[key] = mountPlaces[objectName]['properties'][propertyName]['items']
-        } else if (type.endsWith('[]')) {
-            // if schema(parsed from example) doesn't has this constructure, init
-            if (!mountPlaces[objectName]['properties'][propertyName]) {
-                mountPlaces[objectName]['properties'][propertyName] = {
-                    items: {
-                        type: type.slice(0, -2), description: i.description,
-                        // default: i.defaultValue,
-                        example: i.defaultValue
-                    },
-                    type: 'array'
-                }
-            }
-        } else if (type === 'object') {
-            // if schema(parsed from example) doesn't has this constructure, init
-            if (!mountPlaces[objectName]['properties'][propertyName]) {
-                mountPlaces[objectName]['properties'][propertyName] = { type: 'object', properties: {}, required: [] }
-            }
-
-            // new mount point
-            mountPlaces[key] = mountPlaces[objectName]['properties'][propertyName]
-        } else {
-            mountPlaces[objectName]['properties'][propertyName] = {
-                type,
-                description: i.description,
-                default: i.defaultValue,
-            }
-        }
-        if (!i.optional) {
-            // generate-schema forget init [required]
-            if (mountPlaces[objectName]['required']) {
-                mountPlaces[objectName]['required'].push(propertyName)
-            } else {
-                mountPlaces[objectName]['required'] = [propertyName]
-            }
-        }
-    })
-
-    return parameterInBody
-}
-function generateProps(verb) {
-    // console.log('verb', verb);
-
-    const pathItemObject = {}
-    const parameters = generateParameters(verb)
-    const responses = generateResponses(verb)
-    pathItemObject[verb.type] = {
-        tags: [verb.group],
-        summary: removeTags(verb.name),
-        description: removeTags(verb.title),
-        consumes: [
-            "application/json"
-        ],
-        produces: [
-            "application/json"
-        ],
-        parameters,
-        responses
-    }
-
-    return pathItemObject
-
-}
-
-function generateParameters(verb) {
-    const mixedQuery = []
-    const mixedBody = []
-    const header = verb && verb.header && verb.header.fields.Header || []
-
-    if (verb && verb.parameter && verb.parameter.fields) {
-
-        const Parameter = verb.parameter.fields.Parameter || []
-        const _query = verb.parameter.fields.Query || []
-        const _body = verb.parameter.fields.Body || []
-        mixedQuery.push(..._query)
-        mixedBody.push(..._body)
-        if (verb.type === 'get') {
-            mixedQuery.push(...Parameter)
-        } else {
-            mixedBody.push(...Parameter)
-        }
-    }
-
-    const parameters = []
-    parameters.push(...mixedQuery.map(mapQueryItem))
-    parameters.push(...header.map(mapHeaderItem))
-    parameters.push(generateRequestBody(verb, mixedBody))
-    // console.log('parameters', parameters);
-
-    return parameters
-}
 function generateRequestBody(verb, mixedBody) {
     const bodyParameter = {
-        in: 'body',
+    	description: "Request body",
+    	content: {"application/json":{
         schema: {
             properties: {},
-            type: 'object',
-            required: []
+            type: 'object'
         }
+        }}
     }
-
     if (_.get(verb, 'parameter.examples.length') > 0) {
         for (const example of verb.parameter.examples) {
-            const { code, json } = safeParseJson(example.content)
+            const { code, json } = safeParseJson(example.content, verb)
             const schema = GenerateSchema.json(example.title, json)
-            bodyParameter.schema = schema
+            bodyParameter.content["application/json"].schema = schema
             bodyParameter.description = example.title
         }
     }
 
-    transferApidocParamsToSwaggerBody(mixedBody, bodyParameter)
-
+    transferApidocParamsToSwaggerBody(mixedBody, bodyParameter.content["application/json"])
     return bodyParameter
 }
 
@@ -252,24 +156,19 @@ function generateResponses(verb) {
     const success = verb.success
     const responses = {
         200: {
-            schema: {
-                properties: {},
-                type: 'object',
-                required: []
-            }
+            description: "ok (default)"
         }
     }
     if (success && success.examples && success.examples.length > 0) {
         for (const example of success.examples) {
-            const { code, json } = safeParseJson(example.content)
+            const { code, json } = safeParseJson(example.content, verb)
             const schema = GenerateSchema.json(example.title, json)
-            responses[code] = { schema, description: example.title }
+            delete schema.$schema
+            responses[code] = { content: {"application/json": {schema: schema}}, description: example.title }
         }
 
     }
-
     mountResponseSpecSchema(verb, responses)
-
     return responses
 }
 
@@ -283,26 +182,20 @@ function mountResponseSpecSchema(verb, responses) {
     }
 }
 
-function safeParseJson(content) {
-    // such as  'HTTP/1.1 200 OK\n' +  '{\n' + ...
+function safeParseJson(content, verb) {
     const leftCurlyBraceIndex = content.indexOf('{')
     const mayCodeString = content.slice(0, leftCurlyBraceIndex)
     const mayContentString = content.slice(leftCurlyBraceIndex)
-
     const mayCodeSplit = mayCodeString.trim().split(' ')
-    const code = mayCodeSplit.length === 3 ? parseInt(mayCodeSplit[1]) : 200
-
+    let match = /HTTP\/.+ (\d+) /.exec(content)
+    const code = match[1] ? parseInt(match[1]) : 200
     let json = {}
     try {
         json = JSON.parse(mayContentString)
     } catch (error) {
-        console.warn('parse error', error)
+        console.warn('parse error at', verb.url, verb.type, error, content)
     }
-
-    return {
-        code,
-        json
-    }
+    return {code, json}
 }
 
 
@@ -321,14 +214,60 @@ function createNestedName(field, defaultObjectName) {
     }
 }
 
-function groupByUrl(apidocJson) {
-    return _.chain(apidocJson)
-        .groupBy("url")
-        .toPairs()
-        .map(function (element) {
-            return _.zipObject(["url", "verbs"], element);
-        })
-        .value();
+function transferApidocParamsToSwaggerBody(apiDocParams, parameterInBody) {
+    let mountPlaces = {
+        '': parameterInBody['schema']
+    }
+    apiDocParams.forEach(i => {
+        const type = i.type.toLowerCase()
+        const key = i.field
+        const nestedName = createNestedName(i.field)
+        const { objectName = '', propertyName } = nestedName
+	if (mountPlaces[objectName]) {
+        if (type.endsWith('object[]')) {
+            // if schema(parsed from example) doesn't has this constructure, init
+            if (!mountPlaces[objectName]['properties'][propertyName]) {
+                mountPlaces[objectName]['properties'][propertyName] = { type: 'array', items: { type: 'object', properties: {} } }
+            }
+            // new mount point
+            mountPlaces[key] = mountPlaces[objectName]['properties'][propertyName]['items']
+        } else if (type.endsWith('[]')) {
+            // if schema(parsed from example) doesn't has this constructure, init
+            if (!mountPlaces[objectName]['properties'][propertyName]) {
+                mountPlaces[objectName]['properties'][propertyName] = {
+                    items: {
+                        type: type.slice(0, -2), description: i.description,
+                        // default: i.defaultValue,
+                        example: i.defaultValue
+                    },
+                    type: 'array'
+                }
+            }
+        } else if (type === 'object') {
+            // if schema(parsed from example) doesn't has this constructure, init
+            if (!mountPlaces[objectName]['properties'][propertyName]) {
+                mountPlaces[objectName]['properties'][propertyName] = { type: 'object', properties: {} }
+            }
+            // new mount point
+            mountPlaces[key] = mountPlaces[objectName]['properties'][propertyName]
+        } else {
+            mountPlaces[objectName]['properties'][propertyName] = {
+                type,
+                description: i.description,
+                default: i.defaultValue,
+            }
+        }
+        if (!i.optional) {
+            // generate-schema forget init [required]
+            if (mountPlaces[objectName]['required']) {
+                mountPlaces[objectName]['required'].push(propertyName)
+            } else {
+                mountPlaces[objectName]['required'] = [propertyName]
+            }
+        }
+        }
+    })
+    return parameterInBody
 }
 
 module.exports = {
